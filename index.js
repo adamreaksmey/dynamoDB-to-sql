68acd63a8df3627e449c19e5fbb287d1d626eaca
adamreaksmey <adaminiature@gmail.com>
2024-01-17 17:23:23 +0700

    init converter

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("hello world");
+const fs = require("fs");
+
+const insert_data = (data) => {
+  let queries = [];
+  for (let item of data) {
+    // Prepare column names and values
+    let columns = Object.keys(item).filter((key) => key !== "recordType");
+    let values = columns.map((column) => {
+      let value = item[column];
+      // Check if value is a string, if it is, add single quotes around it
+      if (typeof value === "string") {
+        value = `'${value}'`;
+      }
+      return value;
+    });
+
+    // Build the query
+    let query = `INSERT INTO ${item.recordType} (${columns.join(
+      ", "
+    )}) VALUES (${values.join(", ")});`;
+    queries.push(query);
+  }
+  return queries;
+};
+
+const load_data = (file_path) => {
+  let rawdata = fs.readFileSync(file_path);
+  let data = JSON.parse(rawdata);
+  return data;
+};
+
+const main = () => {
+  const data = load_data("./sources/data.json");
+  const queries = insert_data(data);
+  const sqlContent = queries.join("\n");
+
+  fs.writeFileSync("./generated_sql/migration_queries.sql", sqlContent);
+  console.log("SQL file generated successfully.");
+};
+
+main();
