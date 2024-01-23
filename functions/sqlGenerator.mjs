3ac964198471630656f2d3293b949f8422f07e0a
adamreaksmey <adaminiature@gmail.com>
2024-01-23 16:28:47 +0700

    update

--- functions/sqlGenerator.mjs
+++ functions/sqlGenerator.mjs
@@ -2,7 +2,7 @@ const insert_data = (data) => {
   let queries = [];
   for (let item of data) {
     // Prepare column names and values
-    let columns = Object.keys(item).filter((key) => key !== "recordType");
+    let columns = Object.keys(item).filter((key) => key !== "tableName");
     let values = columns.map((column) => {
       let value = item[column];
       // Check if value is a string, if it is, add single quotes around it
@@ -15,10 +15,7 @@ const insert_data = (data) => {
     });
 
     // Build the query
-    let query = `INSERT INTO ${checkIfRecordTypeExists(
-      item.recordType,
-      item
-    )} (${columns.join(", ")}) VALUES (${values.join(", ")});`;
+    let query = `INSERT INTO ${item.tableName} (${columns.join(", ")}) VALUES (${values.join(", ")});`;
     queries.push(query);
   }
   return queries;
